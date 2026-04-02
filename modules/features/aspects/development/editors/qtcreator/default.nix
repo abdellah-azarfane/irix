c2d664b4fdb17fc61a04bc9e514ab1533716d1ce
Abdellah Azarfane <abdellahazarfane@proton.me>
2026-04-02 14:50:55 +0100

    some pkgs

--- modules/features/aspects/development/editors/qtcreator/default.nix
+++ modules/features/aspects/development/editors/qtcreator/default.nix
@@ -1,9 +1,13 @@
 {
-  flake.nixosModules.qtcreatot = { pkgs, config, ... }: let
+  flake.nixosModules.qtcreator = { pkgs, config, ... }: let
     user = config.preferences.user.name;
   in {
     home-manager.users.${user} = {  
-    home.packages = with pkgs; [ qtcreator ];   
+    home.packages = with pkgs; [ 
+    qtcreator 
+    kdePackages.qtdeclarative
+    kdePackages.qtquick3d
+    ];   
     }; 
   };  
 }
