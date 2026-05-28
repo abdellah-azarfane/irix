8ea15ccfd7d773fafc03dadaced280c95aad9fcb
Abdellah Azarfane <abdellahazarfane@proton.me>
2026-05-28 19:30:49 +0100

    few changes with emacs

--- modules/features/aspects/development/editors/emacs/default.nix
+++ modules/features/aspects/development/editors/emacs/default.nix
@@ -1,16 +1,18 @@
-flake.nixosModules.emacs = { pkgs, config, ... }:
+{
+  flake.nixosModules.emacs : { pkgs, config, ... }:
 let
   user = config.preferences.user.name;
 in {
-  home-manager.users.${user} = {
-    programs.emacs = {
-      enable = true;
-      package = pkgs.emacs-pgtk;
-    };
+   home-manager.users.${user} = {
+     programs.emacs = {
+       enable = true;
+       package = pkgs.emacs-pgtk;
+     };
 
-    services.emacs = {
-      enable = true;
-      client.enable = true;
-    };
+     services.emacs = {
+       enable = true;
+       client.enable = true;
+     };
+   };
   };
-};
+ }
