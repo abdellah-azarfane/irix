df751deb23e675b240f36f496a1bcb4e610687b1
Abdellah Azarfane <abdellahazarfane@proton.me>
2026-06-29 23:45:48 +0100

    virt

--- modules/features/aspects/development/virtua/default.nix
+++ modules/features/aspects/development/virtua/default.nix
@@ -22,9 +22,12 @@
         lsfg-vk
         lsfg-vk-ui
         quickemu
+        spice-gtk
       ];
       # Ensure graphics drivers are actually enabled
       hardware.graphics.enable = lib.mkDefault true;
+      virtualisation.libvirtd.enable = true;
+      users.users.${user}.extraGroups = [ "libvirtd" "kvm" ];
       services.zerotierone.enable = true;
       virtualisation.docker = {
         enable = true;
