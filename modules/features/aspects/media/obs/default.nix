b033648721e32e82a130e4cdbdba87b606db8e18
Abdellah Azarfane <abdellahazarfane@proton.me>
2026-06-29 21:35:43 +0100

    obs

--- modules/features/aspects/media/obs/default.nix
+++ modules/features/aspects/media/obs/default.nix
@@ -1,35 +1,36 @@
 flake.nixosModules.obs =
-    { pkgs, inputs, ... }:
+{ pkgs, inputs, ... }:
 
-    let
-      obsBase =
-        pkgs.wrapOBS.override
-          {
-            obs-studio = pkgs.obs-studio.override { cudaSupport = true; };
-          }
-          {
-            plugins = with pkgs.obs-studio-plugins; [
-              obs-pipewire-audio-capture # For PipeWire audio capture
-              wlrobs # For Wayland screen capture
-              obs-vkcapture # For Vulkan screen capture
-              aitum-stream-suite # For Aitum stream suite
-              obs-multi-rtmp # For multi RTMP output
-              obs-backgroundremoval # For background removal
-              obs-move-transition # For move transition
-              obs-3d-effects # For 3D effects
-            ];
-          };
-
-      obsWrapped = inputs.wrapper-modules.lib.wrapPackage {
-        inherit pkgs;
-        modules = [
-          {
-            package = obsBase;
-            env.LD_LIBRARY_PATH = "/run/opengl-driver/lib"; # for nvidia and nixos i hate this
-          }
+let
+  obsBase =
+    pkgs.wrapOBS.override
+      {
+        obs-studio = pkgs.obs-studio.override { cudaSupport = true; };
+      }
+      {
+        plugins = with pkgs.obs-studio-plugins; [
+          obs-pipewire-audio-capture # For PipeWire audio capture
+          wlrobs # For Wayland screen capture
+          obs-vkcapture # For Vulkan screen capture
+          aitum-stream-suite # For Aitum stream suite
+          obs-multi-rtmp # For multi RTMP output
+          obs-backgroundremoval # For background removal
+          obs-move-transition # For move transition
+          obs-3d-effects # For 3D effects
         ];
       };
-    in
-    {
-      environment.systemPackages = [ obsWrapped ];
-    };
+
+  obsWrapped = inputs.wrapper-modules.lib.wrapProgram {
+    inherit pkgs;
+    modules = [
+      {
+        package = obsBase;
+        env.LD_LIBRARY_PATH = "/run/opengl-driver/lib"; # for nvidia and nixos i hate this
+      }
+    ];
+  };
+in
+{
+  environment.systemPackages = [ obsWrapped ];
+}
+}
